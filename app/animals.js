aee5244bb910a796a9eb98cc9e3ef5e9184ccae0
ILikeSarcasm <ilikesarcasm01@gmail.com>
2022-02-14 17:02:45 +0100

    Testing With Images

--- app/animals.js
+++ app/animals.js
@@ -10,23 +10,26 @@ const web3 = new Web3(process.env.RPC_PROVIDER);
 const wtanimalAddress = process.env.WTANIMAL_CONTRACT;
 const wtanimalContract = new web3.eth.Contract(wtanimalABI, wtanimalAddress);
 
-export async function tokenURI(tokenID, req, res) {
+export function tokenURI(tokenID, req, res) {
     var metadataPath = `./public/metadata/${tokenID}.json`;
     if (tokenID <= 50000) {
         if (!fs.existsSync(metadataPath)) {
-            wtanimalContract.methods.tokenTraits(tokenID).call((err, result) => {
+            wtanimalContract.methods.tokenTraits(tokenID).call(async (err, result) => {
                 if (err) {
                     console.log(`animals.js:tokenURI ${err}`);
                     res.status(200).json({ error: err });
                     return;
                 }
 
-                var tokenImagePath = generateTokenImage(tokenID, result);
-                
+                var tokenImagePath = await generateTokenImage(tokenID, result);
+                var base64 = new Buffer(fs.readFileSync(`${process.cwd()}/public/images/wtanimalsSmall/${tokenID}.png`)).toString('base64');
+
                 var json = {
                     name: `${result.isSheep ? 'Sheep': 'Wolf'} #${tokenID}`,
                     description: 'Wolf Town NFT collection.',
-                    image: `${process.env.URL}images/wtanimals/${tokenID}.png`,
+                    image: `data:image/svg+xml;base64,${base64}`,
+                    // image: `${process.env.URL}images/wtanimals/${tokenID}.png`,
+                    imageSmall: `${process.env.URL}images/wtanimalsSmall/${tokenID}.png`,
                     attributes: [
                         { trait_type: 'fur', value: result['fur'] },
                         { trait_type: 'head', value: result['head'] },
@@ -64,46 +67,48 @@ export async function tokenURI(tokenID, req, res) {
 }
 
 export function generateTokenImage(tokenID, traits) {
-    var bodyPartsPath = `${process.cwd()}/public/images/bodyParts/`;
-    var tokenImagePath = `${process.cwd()}/public/images/wtanimals/${tokenID}.png`;
-    var tokenSmallImagePath = `${process.cwd()}/public/images/wtanimalsSmall/${tokenID}.png`;
+    return new Promise( (resolve, reject) => {
+        var bodyPartsPath = `${process.cwd()}/public/images/bodyParts/`;
+        var tokenImagePath = `${process.cwd()}/public/images/wtanimals/${tokenID}.png`;
+        var tokenSmallImagePath = `${process.cwd()}/public/images/wtanimalsSmall/${tokenID}.png`;
 
-    if (!fs.existsSync(tokenImagePath)) {
-        if (traits.isSheep) {
-            sharp(`${bodyPartsPath}0/${bodyPartsData[0][parseInt(traits.fur)].name}.png`)
-                .composite([
-                    { input: `${bodyPartsPath}1/${bodyPartsData[1][parseInt(traits.head)].name}.png` },
-                    // GOLD EARS ARE BUGGED (3, 4, 5)
-                    { input: `${bodyPartsPath}2/${bodyPartsData[2][[3, 4, 5].indexOf(parseInt(traits.ears)) == -1 ? parseInt(traits.ears) : 0].name}.png` },
-                    { input: `${bodyPartsPath}3/${bodyPartsData[3][parseInt(traits.eyes)].name}.png` },
-                    { input: `${bodyPartsPath}4/${bodyPartsData[4][parseInt(traits.nose)].name}.png` },
-                    { input: `${bodyPartsPath}5/${bodyPartsData[5][parseInt(traits.mouth)].name}.png` },
-                    { input: `${bodyPartsPath}7/${bodyPartsData[7][parseInt(traits.feet)].name}.png` }
-                ])
-                .toFile(tokenSmallImagePath)
-                .then(() => {
-                    sharp(tokenSmallImagePath)
-                        .resize({ width: 320, height: 320, kernel: 'nearest' })
-                        .toFile(tokenImagePath);
-                });
-        } else {
-            sharp(`${bodyPartsPath}9/${bodyPartsData[9][parseInt(traits.fur)].name}.png`)
-                .composite([
-                    { input: `${bodyPartsPath}12/${bodyPartsData[12][parseInt(traits.eyes)].name}.png` },
-                    { input: `${bodyPartsPath}14/${bodyPartsData[14][parseInt(traits.nose)].name}.png` },
-                    { input: `${bodyPartsPath}15/${bodyPartsData[15][parseInt(traits.neck)].name}.png` },
-                    { input: `${bodyPartsPath}10/${bodyPartsData[10][parseInt(traits.alpha)].name}.png` }
-                ])
-                .toFile(tokenSmallImagePath)
-                .then(() => {
-                    sharp(tokenSmallImagePath)
-                        .resize({ width: 320, height: 320, kernel: 'nearest' })
-                        .toFile(tokenImagePath);
-                });
+        if (!fs.existsSync(tokenImagePath)) {
+            if (traits.isSheep) {
+                sharp(`${bodyPartsPath}0/${bodyPartsData[0][parseInt(traits.fur)].name}.png`)
+                    .composite([
+                        { input: `${bodyPartsPath}1/${bodyPartsData[1][parseInt(traits.head)].name}.png` },
+                        // GOLD EARS ARE BUGGED (3, 4, 5)
+                        { input: `${bodyPartsPath}2/${bodyPartsData[2][[3, 4, 5].indexOf(parseInt(traits.ears)) == -1 ? parseInt(traits.ears) : 0].name}.png` },
+                        { input: `${bodyPartsPath}3/${bodyPartsData[3][parseInt(traits.eyes)].name}.png` },
+                        { input: `${bodyPartsPath}4/${bodyPartsData[4][parseInt(traits.nose)].name}.png` },
+                        { input: `${bodyPartsPath}5/${bodyPartsData[5][parseInt(traits.mouth)].name}.png` },
+                        { input: `${bodyPartsPath}7/${bodyPartsData[7][parseInt(traits.feet)].name}.png` }
+                    ])
+                    .toFile(tokenSmallImagePath)
+                    .then(() => {
+                        sharp(tokenSmallImagePath)
+                            .resize({ width: 320, height: 320, kernel: 'nearest' })
+                            .toFile(tokenImagePath)
+                            .then(() => resolve(tokenImagePath));
+                    });
+            } else {
+                sharp(`${bodyPartsPath}9/${bodyPartsData[9][parseInt(traits.fur)].name}.png`)
+                    .composite([
+                        { input: `${bodyPartsPath}12/${bodyPartsData[12][parseInt(traits.eyes)].name}.png` },
+                        { input: `${bodyPartsPath}14/${bodyPartsData[14][parseInt(traits.nose)].name}.png` },
+                        { input: `${bodyPartsPath}15/${bodyPartsData[15][parseInt(traits.neck)].name}.png` },
+                        { input: `${bodyPartsPath}10/${bodyPartsData[10][parseInt(traits.alpha)].name}.png` }
+                    ])
+                    .toFile(tokenSmallImagePath)
+                    .then(() => {
+                        sharp(tokenSmallImagePath)
+                            .resize({ width: 320, height: 320, kernel: 'nearest' })
+                            .toFile(tokenImagePath)
+                            .then(() => resolve(tokenImagePath));
+                    });
+            }
         }
-    }
-
-    return tokenImagePath;
+    });
 }
 
 const animals = { tokenURI, generateTokenImage };
