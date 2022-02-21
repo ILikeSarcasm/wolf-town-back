295d288bf0b7a61e66279f1859250af3d5714206
ILikeSarcasm <ilikesarcasm01@gmail.com>
2022-02-21 16:38:55 +0100

    OldBarn Locked Tokens

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import animals from './app/animals.js';
-import barn from './app/barn.js';
+import oldBarn from './app/oldBarn.js';
 
 const router = express.Router();
 
 router.get('/animals/:id', (req, res, next) => animals.tokenURI(parseInt(req.params.id), req, res));
-router.get('/barn/unstakeOrder1/:address', (req, res, next) => barn.unstakeOrder(req.params.address, true, req, res));
-router.get('/barn/unstakeOrder2/:address', (req, res, next) => barn.unstakeOrder(req.params.address, false, req, res));
+router.get('/barn/unstakeOrder1/:address', (req, res, next) => oldBarn.unstakeOrder(req.params.address, true, req, res));
+router.get('/barn/unstakeOrder2/:address', (req, res, next) => oldBarn.unstakeOrder(req.params.address, false, req, res));
+router.get('/barn/lostTokens/:address', (req, res, next) => oldBarn.lockedTokens(req.params.address, req, res));
 
 export default router;
