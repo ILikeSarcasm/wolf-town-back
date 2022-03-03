fdef2c47cc693236f62fa4a5f5a3d9e20122c99e
ILikeSarcasm <ilikesarcasm01@gmail.com>
2022-03-03 10:40:34 +0100

    Multiple tokens metadata

--- app/animals.js
+++ app/animals.js
@@ -11,63 +11,80 @@ const wtanimalAddress = process.env.WTANIMAL_CONTRACT;
 const wtanimalContract = new web3.eth.Contract(wtanimalABI, wtanimalAddress);
 
 export function tokenURI(tokenID, req, res) {
-    var metadataPath = `./public/metadata/${tokenID}.json`;
-    if (tokenID <= 50000) {
-        if (!fs.existsSync(metadataPath)) {
-            wtanimalContract.methods.tokenTraits(tokenID).call(async (err, result) => {
-                if (err) {
-                    console.log(`animals.js:tokenURI ${err}`);
-                    res.status(200).json({ error: err });
-                    return;
-                }
+    getTokenURI(tokenID)
+        .then(json => res.status(200).json(json))
+        .catch(err => res.status(200).json({ error: err }));
+}
 
-                var tokenImagePath = await generateTokenImage(tokenID, result);
-                var base64 = new Buffer(fs.readFileSync(`${process.cwd()}/public/images/wtanimalsSmall/${tokenID}.png`)).toString('base64');
+export function tokenURIs(tokenIDs, req, res) {
+    var promises = tokenIDs.map(tokenID => getTokenURI(tokenID));
 
-                var json = {
-                    name: `${result.isSheep ? 'Sheep': 'Wolf'} #${tokenID}`,
-                    description: 'Wolf Town NFT collection.',
-                    image: `${process.env.URL}images/animals/${tokenID}.png`,
-                    imageSmall: `data:image/svg;base64,${base64}`,
-                    attributes: [
-                        { trait_type: 'type', value: result.isSheep ? 'Sheep' : 'Wolf' },
-                        { trait_type: 'fur', value: bodyPartsData[result.isSheep ? 0 : 9][parseInt(result['fur'])].name },
-                        { trait_type: 'head', value: bodyPartsData[1][parseInt(result['head'])].name },
-                        { trait_type: 'ears', value: bodyPartsData[2][parseInt(result['ears'])].name },
-                        { trait_type: 'eyes', value: bodyPartsData[result.isSheep ? 3 : 12][parseInt(result['eyes'])].name },
-                        { trait_type: 'nose', value: bodyPartsData[result.isSheep ? 4 : 14][parseInt(result['nose'])].name },
-                        { trait_type: 'mouth', value: bodyPartsData[5][parseInt(result['mouth'])].name },
-                        { trait_type: 'neck', value: result.isSheep ? "None" : bodyPartsData[15][parseInt(result['neck'])].name },
-                        { trait_type: 'feet', value: bodyPartsData[7][parseInt(result['feet'])].name },
-                        { trait_type: 'alpha', value: result.isSheep ? "None" : bodyPartsData[10][parseInt(result['alpha'])].name }
-                    ]
-                };
+    Promise.allSettled(promises).then(results => {
+        var metadatas = results.map(result => result.status == 'fulfilled' ? result.value : result.reason);
+        res.status(200).json(JSON.stringify(metadatas));
+    });
+}
 
-                fs.writeFile(metadataPath, JSON.stringify(json), (err) => {
+function getTokenURI(tokenID) {
+    return new Promise((resolve, reject) => {
+        var metadataPath = `./public/metadata/${tokenID}.json`;
+        if (tokenID <= 50000) {
+            if (!fs.existsSync(metadataPath)) {
+                wtanimalContract.methods.tokenTraits(tokenID).call(async (err, result) => {
                     if (err) {
                         console.log(`animals.js:tokenURI ${err}`);
+                        reject(err);
+                        return;
+                    }
+
+                    var tokenImagePath = await generateTokenImage(tokenID, result);
+                    var base64 = new Buffer(fs.readFileSync(`${process.cwd()}/public/images/wtanimalsSmall/${tokenID}.png`)).toString('base64');
+
+                    var json = {
+                        name: `${result.isSheep ? 'Sheep': 'Wolf'} #${tokenID}`,
+                        description: 'Wolf Town NFT collection.',
+                        image: `${process.env.URL}images/animals/${tokenID}.png`,
+                        imageSmall: `data:image/svg;base64,${base64}`,
+                        attributes: [
+                            { trait_type: 'type', value: result.isSheep ? 'Sheep' : 'Wolf' },
+                            { trait_type: 'fur', value: bodyPartsData[result.isSheep ? 0 : 9][parseInt(result['fur'])].name },
+                            { trait_type: 'head', value: bodyPartsData[1][parseInt(result['head'])].name },
+                            { trait_type: 'ears', value: bodyPartsData[2][parseInt(result['ears'])].name },
+                            { trait_type: 'eyes', value: bodyPartsData[result.isSheep ? 3 : 12][parseInt(result['eyes'])].name },
+                            { trait_type: 'nose', value: bodyPartsData[result.isSheep ? 4 : 14][parseInt(result['nose'])].name },
+                            { trait_type: 'mouth', value: bodyPartsData[5][parseInt(result['mouth'])].name },
+                            { trait_type: 'neck', value: result.isSheep ? "None" : bodyPartsData[15][parseInt(result['neck'])].name },
+                            { trait_type: 'feet', value: bodyPartsData[7][parseInt(result['feet'])].name },
+                            { trait_type: 'alpha', value: result.isSheep ? "None" : bodyPartsData[10][parseInt(result['alpha'])].name }
+                        ]
+                    };
+
+                    fs.writeFile(metadataPath, JSON.stringify(json), (err) => {
+                        if (err) {
+                            console.log(`animals.js:tokenURI ${err}`);
+                        }
+                    });
+
+                    resolve(json);
+                });
+            } else {
+                fs.readFile(metadataPath, (err, json) => {
+                    if (err) {
+                        console.log(`animals.js:tokenURI ${err}`);
+                        reject(err);
+                    } else {
+                        resolve(JSON.parse(json));
                     }
                 });
-
-                res.status(200).json(json);
-            });
+            }
         } else {
-            fs.readFile(metadataPath, (err, json) => {
-                if (err) {
-                    console.log(`animals.js:tokenURI ${err}`);
-                    res.status(200).json({ error: err });
-                } else {
-                    res.status(200).json(JSON.parse(json));
-                }
-            });
+            reject('Token does not exist.');
         }
-    } else {
-        res.status(200).json({ error: 'Token does not exist.' });
-    }
+    });
 }
 
 export function generateTokenImage(tokenID, traits) {
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         var wtanimalsLogo = `${process.cwd()}/public/images/wtanimalsLogo.png`;
         var bodyPartsPath = `${process.cwd()}/public/images/bodyParts/`;
         var tokenImagePath = `${process.cwd()}/public/images/animals/${tokenID}.png`;
@@ -114,6 +131,6 @@ export function generateTokenImage(tokenID, traits) {
     });
 }
 
-const animals = { tokenURI, generateTokenImage };
+const animals = { tokenURI, tokenURIs, generateTokenImage };
 
 export default animals;
--- router.js
+++ router.js
@@ -5,6 +5,7 @@ import oldBarn from './app/oldBarn.js';
 const router = express.Router();
 
 router.get('/animals/:id', (req, res, next) => animals.tokenURI(parseInt(req.params.id), req, res));
+router.get('/animals', (req, res, next) => animals.tokenURIs(JSON.parse(decodeURIComponent(req.query.ids)), req, res));
 router.get('/barn/unstakeOrder1/:address', (req, res, next) => oldBarn.unstakeOrder(req.params.address, true, req, res));
 router.get('/barn/unstakeOrder2/:address', (req, res, next) => oldBarn.unstakeOrder(req.params.address, false, req, res));
 router.get('/barn/lostTokens/:address', (req, res, next) => oldBarn.lockedTokens(req.params.address, req, res));
